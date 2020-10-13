f987b46700bb24fa11a0c9beb34f99ce715e24dd
kimwoobin <kwb020312@naver.com>
2020-10-13 12:15:38 +0900

    Multiple Page Pre-Rendering

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,8 +1,21 @@
 import Axios from 'axios'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { Loader } from 'semantic-ui-react'
 import Item from '../../src/components/Item'
 
 export default function ID({item , name}) {
+    const router = useRouter()
+
+    if(router.isFallback) {
+        return (
+            <div style={{padding:'100px 0'}}>
+                <Loader active inline="centered">
+                    Loading
+                </Loader>
+            </div>
+        )
+    }
 
     return (<>{item && <>
         <Head>
@@ -15,12 +28,16 @@ export default function ID({item , name}) {
 }
 
 export async function getStaticPaths() {
+    const apiUrl = process.env.apiUrl
+    const res = await Axios.get(apiUrl)
+    const data = res.data
+  
     return {
-        paths: [
-            { params: { id: '495' }},
-            { params: { id: '488' }},
-            { params: { id: '468' }},
-        ],
+        paths: data.slice(0,9).map(item => ({
+            params: {
+                id: item.id.toString()
+            }
+        })),
         fallback: true
     }
 }
